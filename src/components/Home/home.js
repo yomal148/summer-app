c838290ae4cb3fa068e81fc6c0278ee982c6d0c8
yomal148 <amaltharyan@gmail.com>
2022-07-15 14:22:26 -0400

    Implementing React Text style

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,14 +1,32 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import './index.css'
-class Home extends Component {
-    render() {
+import { Text, StyleSheet } from "react-native";
+
+const Home = () => {
+    const [titleText, setTitleText] = useState("Bird's Nest");
+    const bodyText = "This is not really a bird nest.";
+
+    const onPressTitle = () => {
+        setTitleText("Bird's Nest [pressed]");
+    };
         return (
-            <div className="home">
+            <Text>
+                <Text onPress={onPressTitle()}>
+                    {titleText}
+                    {"\n"}
+                    {"\n"}
+                </Text>
+            <Text numberOfLines={5}>{bodyText}</Text>
+            </Text>
+
+           /* <div className="home">
+
                 <h1>Discover Your Local Breweries</h1>
                 <div className="Beer-bg"></div>
-            </div>
+            </div>*/
         )
-    }
+
+
 }
 
 export default Home
